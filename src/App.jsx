e126bba7cce54e3dd9e3c19c60e9373b361395d1
rwaikuru <rwaikuru@strathmore.edu>
2024-12-04 16:13:21 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div >
+        <h1 className='text-orange-500 font-bold text-5xl'>
+        welcome to digital learning
+
+        </h1>
+      </div>
+    </main>
+   
+      
+    
+  )
+}
+
+export default App
